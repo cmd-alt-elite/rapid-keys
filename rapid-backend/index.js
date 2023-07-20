432ea25776fb27ef02bfe159e9bf5d705780c3ff
Nishant Luthra <nishant.luthra30@gmail.com>
2023-07-20 13:42:48 +0530

    added basic sockets

--- rapid-backend/index.js
+++ rapid-backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
@@ -8,8 +11,20 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8000;
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+    });
+
+    socket.on('send_stats', (data) => {
+        socket.to(data.room).emit('receive_stats', stats);
+    });
+});
 
 server.listen(PORT, () => {
-    console.log(`Server started.`);
+    console.log(`Server started on port ${PORT}.`);
 })
