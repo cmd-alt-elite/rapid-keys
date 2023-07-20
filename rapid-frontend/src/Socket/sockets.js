a80ef3bfe9dc7a1632b11efa0a23ae5c7da6137f
Sarthak Aggarwal <getsarthakaggarwal@gmail.com>
2023-07-20 18:53:34 +0530

    matchmaking frontend

--- rapid-frontend/src/Socket/sockets.js
+++ rapid-frontend/src/Socket/sockets.js
@@ -1,6 +1,14 @@
 // import "./App.css";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import './sockets.css'
+
+const options = [
+  'easy', 'medium', 'hard'
+];
+const defaultOption = options[1];
 
 const socket = io.connect("https://rapid-keys-back.onrender.com/");
 
@@ -9,49 +17,43 @@ function Sockets() {
   const [room, setRoom] = useState("");
 
   // Messages States
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-  const [senderReceived, setSenderReceived] = useState("");
+  const [username, setUsername] = useState("");
+  const [roomReceived, setRoomReceived] = useState("");
+  let difficulty = "";
 
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room);
+  const makeMatch = () => {
+    if(username !== ""){
+      socket.emit("find_match", username, difficulty);
     }
-  };
-
-  const sendMessage = () => {
-    socket.emit("send_stats", { message, room });
-  };
+  }
 
   useEffect(() => {
-    socket.on("receive_stats", (data, id) => {
-        console.log("received " + data);
-        console.log("sent by  " + id);
-
-      setMessageReceived(data);
-      setSenderReceived(id);
+    socket.on("receive_match", (room) => {
+      console.log("room number alloted is  " + room);
+      setRoom(room);
     });
   }, [socket]);
+
+  const onSelect = (option) => {
+    console.log('You selected ', option.label);
+    difficulty = option;
+}
+
+
   return (
     <div className="App">
       <input
-        placeholder="Room Number..."
+        placeholder="username..."
         onChange={(event) => {
-          setRoom(event.target.value);
+          setUsername(event.target.value);
         }}
+        className="username-field"
       />
-      <button onClick={joinRoom}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
-      <h1> Message Sender:</h1>
-      {senderReceived}
+      <div className="difficulty-dropdown"><Dropdown options={options} value={defaultOption} placeholder="Select an option" onChange={onSelect} /></div>
+      
+      <button onClick={makeMatch} className="match-button"> Enter Matchmaking!</button>
+      <h3> Alloted Room :</h3>
+      {roomReceived}
     </div>
   );
 }
