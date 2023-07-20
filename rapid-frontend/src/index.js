3e3cb99ba2adf4e2f19d9c05360a17afc1cde480
Sarthak Aggarwal <getsarthakaggarwal@gmail.com>
2023-07-20 15:50:30 +0530

    more config

--- rapid-frontend/src/index.js
+++ rapid-frontend/src/index.js
@@ -9,9 +9,9 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </Provider>
+   </Provider>
 );
