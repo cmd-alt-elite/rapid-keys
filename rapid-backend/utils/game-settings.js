dd91f5a0219da42ac8bdb8253eeaebf4b9041aa0
Nishant Luthra <nishant.luthra30@gmail.com>
2023-07-24 11:43:35 +0530

    changed some game settings

--- rapid-backend/utils/game-settings.js
+++ rapid-backend/utils/game-settings.js
@@ -1,4 +1,4 @@
 export const PROGRESS_UPDATE_TIMEOUT = 1;
-export const GAME_DURATION = 60;
+export const GAME_DURATION = 65;
 export const ROOM_CAPACITY = 3;
-export const LOBBY_WAIT_TIME = 30;
+export const LOBBY_WAIT_TIME = 25;
